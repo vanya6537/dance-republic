666af574db1ee579e9ea827d14d7bccc966b97b4
Netsl <vanya6537@gmail.com>
2020-07-17 18:12:22 +0400

    Implemented LanguageSwitcher

---
+++ components/LanguageSwitcher.js
@@ -0,0 +1,28 @@
+import React, {useContext} from "react";
+import {LANGUAGES} from "../lib/constants";
+import LanguageContext from "./LanguageContext";
+import Link from "next/link";
+import cn from "classnames";
+import {useRouter} from 'next/router';
+
+const LanguageSwitcher = () => {
+    const {language, setLanguage} = useContext(LanguageContext);
+    let nextLanguage = LANGUAGES[(LANGUAGES.indexOf(language) + 1) % LANGUAGES.length];
+    const router = useRouter();
+    return (
+        <Link href={'/' + [nextLanguage, ...router.pathname.split('/').slice(2)].join('/')}>
+            <a
+                className={cn('mute', {
+                    selected: router.route.startsWith('/showcase')
+                })}
+                title="Second Post"
+                onClick={() => setLanguage(nextLanguage)}
+            >
+                path: {router.pathname}
+            </a>
+
+        </Link>
+    );
+};
+
+export default LanguageSwitcher;
