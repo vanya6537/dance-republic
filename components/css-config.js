3602d1400e351e9c8caa3d04299c9f0f84c60620
Netsl <vanya6537@gmail.com>
2020-07-17 18:11:55 +0400

    New CSS Color Codes

--- components/css-config.js
+++ components/css-config.js
@@ -8,3 +8,13 @@ export const COLOR_ERROR = '#FF001F';
 export const COLOR_SUCCESS = '#067DF7';
 
 export const COLOR_CODE_LIGHT = '#000';
+export const COLOR_CODE_DARK = '#111'
+export const COLOR_CODE_BLACK = '#000'
+export const COLOR_CODE_LINK_SELECT = '#68b5fb'
+// export const COLOR_CODE_ = ''
+// export const COLOR_CODE_ = ''
+// export const COLOR_CODE_ = ''
+
+export const COLOR_CODE_RED = '#d65d54';
+export const COLOR_CODE_GREY = '#756d6b';
+export const COLOR_CODE_WHITE = '#ffffff';
