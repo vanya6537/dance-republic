dfb5531f3257256cddff630ce710422108cdc252
Netsl <vanya6537@gmail.com>
2020-07-22 03:18:43 +0400

    restructured

--- components/LanguageSwitcher.js
+++ components/LanguageSwitcher.js
@@ -7,19 +7,24 @@ import {useRouter} from 'next/router';
 const LanguageSwitcher = () => {
     const router = useRouter();
     const language = router.pathname.split('/')[1]
-    let nextLanguage = LANGUAGES[(LANGUAGES.indexOf(language) + 1) % LANGUAGES.length];
-    return (
-        <Link href={'/' + [nextLanguage, ...router.pathname.split('/').slice(2)].join('/')}>
+    // let nextLanguage = LANGUAGES[(LANGUAGES.indexOf(language) + 1) % LANGUAGES.length];
+    const links = LANGUAGES.map(lang => {
+        return <Link href={'/' + [lang, ...router.pathname.split('/').slice(2)].join('/')}>
             <a
                 className={cn('mute', {
                     selected: router.route.startsWith('/showcase')
                 })}
                 title="Second Post"
             >
-                {nextLanguage.toUpperCase()}
+                {lang.toUpperCase()}
             </a>
-
         </Link>
+    })
+    return (<div className='lang-links'>
+            {links}
+
+        </div>
+
     );
 };
 
