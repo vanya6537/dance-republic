262d75182e8b5f6aabd4d737184e835f2e5126bf
Netsl <vanya6537@gmail.com>
2020-07-22 17:12:22 +0400

    fixed phone submission

--- pages/api/phone.js
+++ pages/api/phone.js
@@ -3,15 +3,17 @@ const axios = require('axios')
 const token = process.env.TELEGRAM_BOT_TOKEN
 const telegramUserId = process.env.TELEGRAM_USER_ID
 
-function sendMessage(botToken, chatId, text) {
+function sendMessage(botToken, chatId, textMessage) {
+
     const baseUrl = `https://api.telegram.org/bot${botToken}/sendMessage`
+    const params = {chat_id: parseInt(chatId, 10), text: textMessage}
     return new Promise(resolve => {
-            axios
-                .get(baseUrl, {chat_id: parseInt(chatId, 10), text})
-                .then(resp => {
-                    console.log(resp.data)
-                    resolve()
-                })
+        axios
+            .get(baseUrl, {params})
+            .then(resp => {
+                console.log(resp.data)
+                resolve()
+            })
                 .catch(e => {
                     console.error('Telegram error', e.response.data)
                     resolve()
@@ -25,6 +27,7 @@ module.exports = (request, response) => {
         phone = "Hey there! Simple response!"
     } = request.body
     console.log(request.body)
+    console.log()
     const messageText = `Phone: ${phone}`
     console.log(messageText)
     sendMessage(token, telegramUserId, messageText).then(
